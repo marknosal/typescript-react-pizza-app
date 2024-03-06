c13a0f12ea0fae1866607fa87aee7cbc5e936c92
Mark Nosal <mark.c.nosal@gmail.com>
2024-03-06 17:49:33 -0600

    Add source map

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 const App = () => {
-    return <div>Hello World</div>;
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
+    (window as any).hello()
+    return <div>Hello world</div>;
+  };
+   
+  const rootElement = document.getElementById('root');
+  if (!rootElement) throw new Error('Failed to find the root element');
+  const root = createRoot(rootElement);
+  root.render(<App />);
